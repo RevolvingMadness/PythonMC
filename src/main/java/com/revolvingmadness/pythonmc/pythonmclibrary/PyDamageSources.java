7a9e6363ea9b0705deb69d9f6070418440931b55
Revolving Madness <andyallen216@gmail.com>
2023-06-12 11:33:43 -0500

    Update Entities, Blocks, and DamageSources for 1.20

--- src/main/java/com/revolvingmadness/pythonmc/pythonmclibrary/PyDamageSources.java
+++ src/main/java/com/revolvingmadness/pythonmc/pythonmclibrary/PyDamageSources.java
@@ -5,33 +5,35 @@ import net.minecraft.entity.damage.DamageSources;
 import net.minecraft.world.World;
 
 public enum PyDamageSources {
-	INFIRE, LIGHTNINGBOLT, ONFIRE, LAVA, HOTFLOOR, INWALL, CRAMMING, DROWN, STARVE, CACTUS, FALL, FLYINTOWALL, OUTOFWORLD, GENERIC, MAGIC, WITHER, DRAGONBREATH, DRYOUT, SWEETBERRYBUSH, FREEZE, STALAGMITE;
-	
+	IN_FIRE, LIGHTNING_BOLT, ON_FIRE, LAVA, HOT_FLOOR, IN_WALL, CRAMMING, DROWN, STARVE, CACTUS, FALL, FLY_INTO_WALL, OUT_OF_WORLD, GENERIC, MAGIC, WITHER, DRAGON_BREATH, DRY_OUT, SWEET_BERRY_BUSH, FREEZE, STALAGMITE, OUTSIDE_BORDER, GENERIC_KILL;
+
 	public DamageSource toDamageSource(World world) {
 		DamageSources damageSources = world.getDamageSources();
-		
+
 		return switch (this) {
-			case INFIRE -> damageSources.inFire();
-			case LIGHTNINGBOLT -> damageSources.lightningBolt();
-			case ONFIRE -> damageSources.onFire();
+			case IN_FIRE -> damageSources.inFire();
+			case LIGHTNING_BOLT -> damageSources.lightningBolt();
+			case ON_FIRE -> damageSources.onFire();
 			case LAVA -> damageSources.lava();
-			case HOTFLOOR -> damageSources.hotFloor();
-			case INWALL -> damageSources.inWall();
+			case HOT_FLOOR -> damageSources.hotFloor();
+			case IN_WALL -> damageSources.inWall();
 			case CRAMMING -> damageSources.cramming();
 			case DROWN -> damageSources.drown();
 			case STARVE -> damageSources.starve();
 			case CACTUS -> damageSources.cactus();
 			case FALL -> damageSources.fall();
-			case FLYINTOWALL -> damageSources.flyIntoWall();
-			case OUTOFWORLD -> damageSources.outOfWorld();
+			case FLY_INTO_WALL -> damageSources.flyIntoWall();
+			case OUT_OF_WORLD -> damageSources.outOfWorld();
 			case GENERIC -> damageSources.generic();
 			case MAGIC -> damageSources.magic();
 			case WITHER -> damageSources.wither();
-			case DRAGONBREATH -> damageSources.dragonBreath();
-			case DRYOUT -> damageSources.dryOut();
-			case SWEETBERRYBUSH -> damageSources.sweetBerryBush();
+			case DRAGON_BREATH -> damageSources.dragonBreath();
+			case DRY_OUT -> damageSources.dryOut();
+			case SWEET_BERRY_BUSH -> damageSources.sweetBerryBush();
 			case FREEZE -> damageSources.freeze();
 			case STALAGMITE -> damageSources.stalagmite();
+			case OUTSIDE_BORDER -> damageSources.outsideBorder();
+			case GENERIC_KILL -> damageSources.genericKill();
 		};
 	}
 }
